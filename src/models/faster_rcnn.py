467f7340b5a50cf44ffe726f3838c9d0bb193555
aeg <aeg@amazon.com>
2019-10-17 11:34:17 +1100

    Sample

---
+++ src/models/faster_rcnn.py
@@ -0,0 +1,35 @@
+# *****************************************************************************
+# * Copyright 2019 Amazon.com, Inc. and its affiliates. All Rights Reserved.  *
+#                                                                             *
+# Licensed under the Amazon Software License (the "License").                 *
+#  You may not use this file except in compliance with the License.           *
+# A copy of the License is located at                                         *
+#                                                                             *
+#  http://aws.amazon.com/asl/                                                 *
+#                                                                             *
+#  or in the "license" file accompanying this file. This file is distributed  *
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  *
+#  express or implied. See the License for the specific language governing    *
+#  permissions and limitations under the License.                             *
+# *****************************************************************************
+import torchvision
+from torch import nn
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+
+class FasterRCnn(nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        # load a model pre-trained pre-trained on COCO
+        self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+        # get number of input features for the classifier
+        in_features = self.model.roi_heads.box_predictor.cls_score.in_features
+        # replace the pre-trained head with a new one
+        self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+        print(self.model)
+
+    def forward(self, *input):
+        pass
---
+++ tests/tests_models/test_fasterRCnn.py
@@ -0,0 +1,25 @@
+# *****************************************************************************
+# * Copyright 2019 Amazon.com, Inc. and its affiliates. All Rights Reserved.  *
+#                                                                             *
+# Licensed under the Amazon Software License (the "License").                 *
+#  You may not use this file except in compliance with the License.           *
+# A copy of the License is located at                                         *
+#                                                                             *
+#  http://aws.amazon.com/asl/                                                 *
+#                                                                             *
+#  or in the "license" file accompanying this file. This file is distributed  *
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  *
+#  express or implied. See the License for the specific language governing    *
+#  permissions and limitations under the License.                             *
+# *****************************************************************************
+from unittest import TestCase
+
+from models.faster_rcnn import FasterRCnn
+
+
+class TestFasterRCnn(TestCase):
+    def test_forward(self):
+        num_classes = 10
+        sut = FasterRCnn(num_classes)
+
+        sut()

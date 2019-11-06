6624cd5f3de3fa80f3f5e58545cd67e6c89a4444
aeg <aeg@amazon.com>
2019-11-06 15:38:17 +1100

    Fix signature

--- src/evaluators/iou_matrix.py
+++ src/evaluators/iou_matrix.py
@@ -30,26 +30,26 @@ class IoUMatrix():
         assert g.shape[1] == 4, "Expected 2d tensor of shape ( :, 4) but this tensor has shape {}".format(g.shape)
         assert p.shape[1] == 4, "Expected 2d tensor of shape ( :, 4) but this tensor has shape {}".format(p.shape)
 
-        assert torch.all(g[:, 0] <= g[:, 1]).item(), "Expect all items in index g[:,0] to be less than index g[:,1]"
-        assert torch.all(g[:, 2] <= g[:, 3]).item(), "Expect all items in index g[:,2] to be less than index g[:,3]"
+        assert torch.all(g[:, 0] <= g[:, 2]).item(), "Expect all items in index g[:,0] to be less than index g[:,1]"
+        assert torch.all(g[:, 1] <= g[:, 3]).item(), "Expect all items in index g[:,2] to be less than index g[:,3]"
 
-        assert torch.all(p[:, 0] <= p[:, 1]).item(), "Expect all items in index p[:,0] to be less than index p[:,1]"
-        assert torch.all(p[:, 2] <= p[:, 3]).item(), "Expect all items in index p[:,2] to be less than index p[:,3]"
+        assert torch.all(p[:, 0] <= p[:, 2]).item(), "Expect all items in index p[:,0] to be less than index p[:,1]"
+        assert torch.all(p[:, 1] <= p[:, 3]).item(), "Expect all items in index p[:,2] to be less than index p[:,3]"
 
         g = g.unsqueeze(1)
 
         # Overlap in x
         start_x = torch.max(g[:, :, 0], p[:, 0])
-        end_x = torch.min(g[:, :, 1], p[:, 1])
+        end_x = torch.min(g[:, :, 2], p[:, 2])
 
         # Overlap in y
-        start_y = torch.max(g[:, :, 2], p[:, 2])
+        start_y = torch.max(g[:, :, 1], p[:, 1])
         end_y = torch.min(g[:, :, 3], p[:, 3])
 
         intersection_area = (end_x - start_x) * (end_y - start_y)
 
-        area_g = (g[:, :, 1] - g[:, :, 0]) * (g[:, :, 3] - g[:, :, 2])
-        area_p = (p[:, 1] - p[:, 0]) * (p[:, 3] - p[:, 2])
+        area_g = (g[:, :, 2] - g[:, :, 0]) * (g[:, :, 3] - g[:, :, 1])
+        area_p = (p[:, 2] - p[:, 0]) * (p[:, 3] - p[:, 1])
 
         union_area = area_g + area_p - intersection_area
 

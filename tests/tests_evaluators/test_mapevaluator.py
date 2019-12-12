eeb1b790cd16b5ccb473b6684924fb9edb5970f3
aeg <aeg@amazon.com>
2019-12-12 11:59:30 -0800

    Update tests

--- tests/tests_evaluators/test_mapevaluator.py
+++ tests/tests_evaluators/test_mapevaluator.py
@@ -13,7 +13,6 @@
 #  permissions and limitations under the License.                             *
 # *****************************************************************************
 from unittest import TestCase
-from unittest.mock import MagicMock
 
 import torch
 
@@ -51,9 +50,96 @@ class TestMapEvaluator(TestCase):
         # Assert
         self.assertEqual(expected_map_score, round(actual, 2))
 
-    def test_evaluate_2_boxes(self):
+    def test_evaluate_2_gt_boxes_incorrect_confidence(self):
         """
-        Test case where 2 boxes were predicted and only one matches
+        Test case where 2 gt, 1 boxes were predicted with one matches, and has incorrect confidence score
+        :return:
+        """
+        # Arrange
+
+        sut = MAPEvaluator()
+
+        target = [{"image_id": torch.tensor(1),
+                   "boxes": torch.tensor([[1, 2, 3, 4], [11, 12, 13, 14]]).float(),
+                   "labels": torch.tensor([1, 1]),
+                   "iscrowd": torch.tensor([0, 0])
+                   }]
+
+        # [1, 2, 3, 4]
+        predicted = [{"image_id": torch.tensor(1),
+                      "boxes": torch.tensor([[1, 2, 3, 4]]).float(),
+                      "labels": torch.tensor([1]),
+                      "scores": torch.tensor([0.8])
+                      }]
+        expected_map = .5
+
+        # Act
+        actual = sut(target, predicted)
+
+        # Assert
+        self.assertEqual(round(expected_map, 2), round(actual, 2))
+
+    def test_evaluate_2_gt_2_p_boxes_one_incorrect(self):
+        """
+        Test case where 2 gt, 2 boxes were predicted and only one matches, and has incorrect confidence score
+        :return:
+        """
+        # Arrange
+
+        sut = MAPEvaluator()
+
+        target = [{"image_id": torch.tensor(1),
+                   "boxes": torch.tensor([[1, 2, 3, 4], [11, 12, 13, 14]]).float(),
+                   "labels": torch.tensor([1, 1]),
+                   "iscrowd": torch.tensor([0, 0])
+                   }]
+
+        # [1, 2, 3, 4]
+        predicted = [{"image_id": torch.tensor(1),
+                      "boxes": torch.tensor([[19, 20, 22, 23], [1, 2, 3, 4]]).float(),
+                      "labels": torch.tensor([1, 1]),
+                      "scores": torch.tensor([1, 1])
+                      }]
+        expected_map = .25
+
+        # Act
+        actual = sut(target, predicted)
+
+        # Assert
+        self.assertEqual(round(expected_map, 2), round(actual, 2))
+
+    def test_evaluate_2_gt_2_p_boxes_both_correct_confidence(self):
+        """
+        Test case where 2 gt, 2 boxes were predicted and only one matches, and has incorrect confidence score
+        :return:
+        """
+        # Arrange
+
+        sut = MAPEvaluator()
+
+        target = [{"image_id": torch.tensor(1),
+                   "boxes": torch.tensor([[1, 2, 3, 4], [11, 12, 13, 14]]).float(),
+                   "labels": torch.tensor([1, 1]),
+                   "iscrowd": torch.tensor([0, 0])
+                   }]
+
+        # [1, 2, 3, 4]
+        predicted = [{"image_id": torch.tensor(1),
+                      "boxes": torch.tensor([[11, 12, 13, 14], [1, 2, 3, 4]]).float(),
+                      "labels": torch.tensor([1, 1]),
+                      "scores": torch.tensor([1, 1])
+                      }]
+        expected_map = 1.0
+
+        # Act
+        actual = sut(target, predicted)
+
+        # Assert
+        self.assertEqual(round(expected_map, 2), round(actual, 2))
+
+    def test_evaluate_2_p_boxes_incorrect_confidence(self):
+        """
+        Test case where 2 boxes were predicted and only one matches, and has incorrect confidence score
         :return:
         """
         # Arrange
@@ -67,12 +153,43 @@ class TestMapEvaluator(TestCase):
                    "iscrowd": torch.tensor([0])
                    }]
 
+        # [1, 2, 3, 4]
         predicted = [{"image_id": torch.tensor(1),
-                      "boxes": torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]]).float(),
+                      "boxes": torch.tensor([[5, 6, 7, 8], [1, 2, 3, 4], ]).float(),
                       "labels": torch.tensor([1, 1]),
-                      "scores": torch.tensor([1.0, 1.0])
+                      "scores": torch.tensor([0.8, 0.7])
                       }]
-        expected_map = .667
+        expected_map = .5
+
+        # Act
+        actual = sut(target, predicted)
+
+        # Assert
+        self.assertEqual(round(expected_map, 2), round(actual, 2))
+
+    def test_evaluate_2_p_boxes_correct_confidence(self):
+        """
+        Test case where 2 boxes were predicted and only one matches, and has correct confidence score
+        :return:
+        """
+        # Arrange
+
+        sut = MAPEvaluator()
+
+        target = [{"image_id": torch.tensor(1),
+                   "boxes": torch.tensor([[1, 2, 3, 4]]).float(),
+                   "labels": torch.tensor([1]),
+                   "area": torch.tensor([1.0]),
+                   "iscrowd": torch.tensor([0])
+                   }]
+
+        # [1, 2, 3, 4]
+        predicted = [{"image_id": torch.tensor(1),
+                      "boxes": torch.tensor([[5, 6, 7, 8], [1, 2, 3, 4], ]).float(),
+                      "labels": torch.tensor([1, 1]),
+                      "scores": torch.tensor([0.5, 0.7])
+                      }]
+        expected_map = 1.0
 
         # Act
         actual = sut(target, predicted)

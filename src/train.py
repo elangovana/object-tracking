5b1df39cc40dcc63aad6cd26e273d576c89007e2
aeg <aeg@amazon.com>
2019-12-13 12:25:00 -0800

    Clean up train

--- src/train.py
+++ src/train.py
@@ -60,24 +60,19 @@ class Train:
         self.logger.info("Running training...")
 
         model.to(device=self.device)
-        best_loss = None
         best_score = None
         patience = 0
 
         result_logs = []
 
         for e in range(self.epochs):
-            total_loss = 0
+            total_train_loss = 0
             for i, (images, targets) in enumerate(train_data):
                 # Set up train mode
                 model.train()
 
                 images = list(image.to(self.device) for image in images)
                 targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
-                # [{} for i in range(len(images))]
-                # for k, v in targets.items():
-                #     for i, v_i in enumerate(v):
-                #         targets_transformed[i][k] = v_i
 
                 # Forward pass
                 loss_dict = model(images, targets)
@@ -95,12 +90,12 @@ class Train:
                 self.logger.debug(
                     "Batch {}/{}, loss {}".format(i, e, loss.item()))
 
-                total_loss += loss.item()
+                total_train_loss += loss.item()
 
-            self._compute_validation_loss(train_data, model)
+            # compute train score
             train_target, train_predictions, train_score = self._compute_validation_loss(train_data, model)
 
-            # Validation loss
+            # Validation score
             val_target, val_predictions, val_score = self._compute_validation_loss(val_data, model)
 
             # Save snapshots
@@ -109,52 +104,20 @@ class Train:
                     "Snapshotting as current score {} is > previous best {}".format(val_score, best_score))
                 self.snapshotter.save(model, output_dir=output_dir, prefix="snapshot_")
                 best_score = val_score
-                # best_loss = val_loss
-                # Reset patience if loss decreases
+
                 patience = 0
-                # # score is the same but lower loss
-                # elif val_score == best_score and best_loss is not None and val_loss < best_loss:
-                #     self.logger.info(
-                #         "Snapshotting as current loss {} is < previous best {} for score {}".format(val_loss, best_loss,
-                #                                                                                     val_score))
-                #     self.snapshotter.save(model, output_dir=output_dir, prefix="snapshot_")
-                #     best_loss = val_loss
-                #     # Reset patience if loss decreases
-                #     patience = 0
-                # else:
-                # No increase in best loss so increase patience counter
+
+            else:
                 patience += 1
 
-            # print("###score: train_loss### {}".format(train_loss))
-            # print("###score: val_loss### {}".format(val_loss))
-            # print("###score: val_loss_std### {}".format(val_loss_std))
-            # print("###score: train_loss_std### {}".format(train_loss_std))
+            print("###score: train_loss### {}".format(total_train_loss))
             print("###score: train_score### {}".format(train_score))
             print("###score: val_score### {}".format(val_score))
 
-
-            # print and store run logs
-            # self.logger.info(
-            #     "epoch: {}, train_loss {}, val_loss {}, val_loss_mean {}, train_loss_mean {}, val_loss_std {}, train_loss_std {}, train_score {}, val_score {}".format(
-            #         e, train_loss,
-            #         val_loss,
-            #         val_loss_mean, train_loss_mean, val_loss_std, train_loss_std,
-            #         train_score,
-            #         val_score))
-            # result_logs.append([e, train_loss,
-            #                     val_loss,
-            #                     val_loss_mean, train_loss_mean, val_loss_std, train_loss_std,
-            #                     train_score,
-            #                     val_score])
-
             self.logger.info(
-                "epoch: {},  train_score {}, val_score {}".format(
-                    e,
-                    train_score,
-                    val_score))
-            result_logs.append([e,
-                                train_score,
-                                val_score])
+                "epoch: {}, train_loss {}, train_score {}, val_score {}".format(e, total_train_loss, train_score,
+                                                                                val_score))
+            result_logs.append([e, train_score, val_score])
 
             # Early stopping
             if self.early_stopping and patience > self.patience_epochs:
@@ -170,36 +133,22 @@ class Train:
         # Model Eval mode
         model.eval()
 
-        losses = []
-
-        # No grad
         predictions = []
         target_items = []
+
+        # No grad
         with torch.no_grad():
             for i, (images, targets) in enumerate(data):
-
                 images = list(image.to(self.device) for image in images)
                 targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
-                # [{} for i in range(len(images))]
-                # for k, v in targets.items():
-                #     for i, v_i in enumerate(v):
-                #         targets_transformed[i][k] = v_i
-
-                #   target_items.append(targets)
 
                 # Forward pass
                 # in eval model only gets the predictions and not loss..
-                predidcted_batch = model(images, targets)
+                predicted_batch = model(images, targets)
 
                 self.logger.debug("Computing loss function: ")
 
-                # # Check for Nans
-                # if loss.ne(loss) :
-                #     self.logger.warning(
-                #         "All outputs are NaNs in loss in batch{}.. This could be because of exploding or vanishing gradients".format(
-                #             i))
-
-                predictions.extend(predidcted_batch)
+                predictions.extend(predicted_batch)
                 target_items.extend(targets)
 
         score = self._get_score(target_items, predictions)
